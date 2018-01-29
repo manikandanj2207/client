963fcacef9046d2b972f9b3d46d9791613462d7d
manikandanj2207 <manikandan.jeeva@ymail.com>
2018-01-29 11:01:05 +0530

    01292018 : 11.01am

---
+++ src/index.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import { Router, Route, IndexRoute, browserHistory } from 'react-router';
+
+import App from './components/app';
+import reducers from './reducers';
+
+/*const createStoreWithMiddleware = applyMiddleware()(createStore);*/
+
+const store = createStore(reducers, composeWithDevTools(applyMiddleware()));
+
+
+ReactDOM.render(
+  /* <Provider store={createStoreWithMiddleware(reducers)}> */
+  <Provider store={store} >
+    <Router history={browserHistory}>
+      <Route path="/" component={App}>
+      </Route>
+    </Router>
+  </Provider>
+  , document.querySelector('.container'));
