aedd5e65016659ec1ac63488d5a8e42226faae10
manikandanj2207 <manikandan.jeeva@ymail.com>
2018-01-31 20:12:33 +0530

    01312018 : 08.12am

--- src/index.js
+++ src/index.js
@@ -5,26 +5,41 @@ import { createStore, applyMiddleware } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import { Router, Route, IndexRoute, browserHistory } from 'react-router';
 import reduxThunk from 'redux-thunk';
+import { AUTH_USER } from './actions/types';
 
 import App from './components/app';
 import Signin from './components/auth/signin';
 import Signout from './components/auth/signout';
 import Signup from './components/auth/signup';
+import Feature from './components/feature';
+import Require_Auth from './components/auth/require_auth';
+import Welcome from './components/welcome';
 import reducers from './reducers';
 
 /*const createStoreWithMiddleware = applyMiddleware()(createStore);*/
 
 const store = createStore(reducers, composeWithDevTools(applyMiddleware(reduxThunk)));
 
+const token = localStorage.getItem('token');
+/* If we have a token, consider the user to be signed in */
+
+if (token){
+  /* We need to update application state */
+  store.dispatch({
+    type: AUTH_USER
+  });
+}
 
 ReactDOM.render(
   /* <Provider store={createStoreWithMiddleware(reducers)}> */
   <Provider store={store} >
     <Router history={browserHistory}>
       <Route path="/" component={App}>
+        <IndexRoute component={Welcome}/>
         <Route path="signin" component={Signin}/>
         <Route path="signout" component={Signout}/>
         <Route path="signup" component={Signup}/>
+        <Route path="feature" component={Require_Auth(Feature)}/>
       </Route>
     </Router>
   </Provider>
